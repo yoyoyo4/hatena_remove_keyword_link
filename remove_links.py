6815898e0e75c13290a3e91c7b7038ba2422e108
yoyoyo4 <34539592+yoyoyo4@users.noreply.github.com>
2021-04-19 01:45:20 +0900

    エラー処理追加

--- remove_links.py
+++ remove_links.py
@@ -10,23 +10,31 @@ import re
 import requests, pyperclip
 from bs4 import BeautifulSoup
 
-blog_url = input("キーワードリンクを除去したいはてなブログ記事のURLを入力し、Enterキーを押してください : ")
-res = requests.get(blog_url)
-soup = BeautifulSoup(res.text, 'html.parser')
+blog_url = input("キーワードリンクを除去するはてなブログ記事のURLを入力し、Enterキーを押してください : ")
+try:
+    res = requests.get(blog_url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+except:
+    _ = input("URLにアクセスできません。Enterキーを押して終了してください")
+    exit()
 
-# 記事サイトソースの本文(<div class="entry-content"> の内部)は、HTML編集で表示される内容とは異なる
-_ = input("記事の｢HTML編集｣本文をコピーし、Enterキーを押してください")
-blog_html = pyperclip.paste()
-soup_html = BeautifulSoup(blog_html, 'html.parser')
+try:
+    # 記事サイトソースの本文(<div class="entry-content"> の内部)は、HTML編集で表示される内容とは異なる
+    _ = input("記事の｢HTML編集｣本文をコピーし、Enterキーを押してください")
+    blog_html = pyperclip.paste()
+    soup_html = BeautifulSoup(blog_html, 'html.parser')
 
-# キーワードリンクが張られた単語一覧を取得し、辞書順に並べる
-linked_keywords = sorted(list(set(url.get_text() for url in soup.find_all('a') if "http://d.hatena.ne.jp/keyword/" in str(url.get('href')))))
+    # キーワードリンクが張られた単語一覧を取得し、辞書順に並べる
+    linked_keywords = sorted(list(set(url.get_text() for url in soup.find_all('a') if "http://d.hatena.ne.jp/keyword/" in str(url.get('href')))))
 
-for lk in linked_keywords:
-    for with_lk in soup_html.find_all(text=re.compile(".*" + lk + ".*")):
-        s = with_lk.string
-        if s[0] != "[" or s[-1] != "]": # Twitter埋め込みなどのコマンドを置換候補から除く
-            with_lk.replace_with(s.replace("[]" + lk + "[]", lk).replace(lk, "[]" + lk + "[]")) # リンク付き単語を[]で囲む。2重付与しないよう既に囲まれたものを戻してから
+    for lk in linked_keywords:
+        for with_lk in soup_html.find_all(text=re.compile(".*" + lk + ".*")):
+            s = with_lk.string
+            if s[0] != "[" or s[-1] != "]": # Twitter埋め込みなどのコマンドを置換候補から除く
+                with_lk.replace_with(s.replace("[]" + lk + "[]", lk).replace(lk, "[]" + lk + "[]")) # リンク付き単語を[]で囲む。2重付与しないよう既に囲まれたものを戻してから
 
-pyperclip.copy(str(soup_html))
-_ = input("リンク除去処理済のHTML本文がコピーされました。Enterキーを押して終了してください")
+    pyperclip.copy(str(soup_html))
+    _ = input("リンク除去処理済のHTML本文がコピーされました。Enterキーを押して終了してください")
+
+except:
+    _ = input("予期せぬエラーが発生しました。Enterキーを押して終了してください")
