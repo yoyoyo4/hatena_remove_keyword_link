290b9acdee9d0732fadabdb2c5213e7f3e6e032d
yoyoyo4 <34539592+yoyoyo4@users.noreply.github.com>
2023-04-23 13:34:00 +0900

    http → https

--- hatena_remove_links.py
+++ hatena_remove_links.py
@@ -11,20 +11,22 @@ import requests, pyperclip
 from bs4 import BeautifulSoup
 import PySimpleGUI as sg
 
-layout = [[sg.Text('キーワードリンクを除去するはてなブログ記事のURLを入力し、OKを押してください')],
-            [sg.Input(key='-IN-')],
-            [sg.Button('OK', bind_return_key=True)]]
-window = sg.Window('hatena_remove_links', layout)
+layout = [
+    [sg.Text("キーワードリンクを除去するはてなブログ記事のURLを入力し、OKを押してください")],
+    [sg.Input(key="-IN-")],
+    [sg.Button("OK", bind_return_key=True)],
+]
+window = sg.Window("hatena_remove_links", layout)
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         sys.exit()
     elif event == "OK":
-        blog_url = values['-IN-']
+        blog_url = values["-IN-"]
         try:
             res = requests.get(blog_url)
-            soup = BeautifulSoup(res.text, 'html.parser')
+            soup = BeautifulSoup(res.text, "html.parser")
             window.close()
             break
         except:
@@ -32,29 +34,42 @@ while True:
 
 
 # 記事サイトソースの本文(<div class="entry-content"> の内部)は、HTML編集で表示される内容と異なる。直接貼り付ける必要あり
-layout = [[sg.Text('記事の｢HTML編集｣本文を入力し、OKを押してください')],
-        [sg.Multiline(size=(110, 30), key='-IN-')],
-        [sg.Button('OK', bind_return_key=True)]]
-window = sg.Window('hatena_remove_links', layout)
+layout = [
+    [sg.Text("記事の｢HTML編集｣本文を入力し、OKを押してください")],
+    [sg.Multiline(size=(110, 30), key="-IN-")],
+    [sg.Button("OK", bind_return_key=True)],
+]
+window = sg.Window("hatena_remove_links", layout)
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         sys.exit()
     elif event == "OK":
-        blog_html = values['-IN-']
-        soup_html = BeautifulSoup(blog_html, 'html.parser')
+        blog_html = values["-IN-"]
+        soup_html = BeautifulSoup(blog_html, "html.parser")
 
         # キーワードリンクが張られた単語一覧を取得し、文字数順に並べる
         # 文字列AとABが両方ともキーワードリンク対象のとき、Aから先に検索してAB→[]A[]BとするとABの検索に引っかからなくなり、2重付与を防げる
-        linked_keywords = sorted(list(set(url.get_text() for url in soup.find_all('a') if "http://d.hatena.ne.jp/keyword/" in str(url.get('href')))), key=len)
+        linked_keywords = sorted(
+            list(
+                set(
+                    url.get_text()
+                    for url in soup.find_all("a")
+                    if "https://d.hatena.ne.jp/keyword/" in str(url.get("href"))
+                )
+            ),
+            key=len,
+        )
 
         for lk in linked_keywords:
             for with_lk in soup_html.find_all(text=re.compile(".*" + lk + ".*")):
                 s = with_lk.string
-                if s[0] != "[" or s[-1] != "]": # Twitter埋め込みなどのコマンドを置換候補から除く
-                    with_lk.replace_with(s.replace("[]" + lk + "[]", lk).replace(lk, "[]" + lk + "[]")) # リンク付き単語を[]で囲む。2重付与しないよう既に囲まれたものを戻してから
+                if s[0] != "[" or s[-1] != "]":  # Twitter埋め込みなどのコマンドを置換候補から除く
+                    with_lk.replace_with(
+                        s.replace("[]" + lk + "[]", lk).replace(lk, "[]" + lk + "[]")
+                    )  # リンク付き単語を[]で囲む。2重付与しないよう既に囲まれたものを戻してから
 
         pyperclip.copy(str(soup_html))
-        window['-IN-'].update(str(soup_html))
+        window["-IN-"].update(str(soup_html))
         sg.popup("リンク除去処理済のHTML本文がコピーされました\n｢HTML編集｣本文に上書き貼り付けしてください", no_titlebar=True)
